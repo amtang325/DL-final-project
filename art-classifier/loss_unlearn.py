b1f13ee37161be802b72242bdb1a4a82575bb29b
Zini Chakraborty <zinichakraborty@gmail.com>
2025-04-27 16:33:42 -0400

    sample from retain dataset

--- art-classifier/loss_unlearn.py
+++ art-classifier/loss_unlearn.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import torch
+from torch.utils.data import SubsetRandomSampler, DataLoader
 
 from typing import Tuple, Dict, Any
 from tqdm import tqdm
@@ -23,10 +24,13 @@ from torch.utils.data import DataLoader
 def unlearn_loss(data_dir, model, optimizer, batch_size, num_epochs):
     train_data_transforms = train_transforms(size=(224, 224))
     val_data_transforms = test_transforms(size=(224, 224))
+    
     train_dataset = ArtLoader(data_dir, split="train", transform=train_data_transforms, train_file="genre_train_with_dali.csv")
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    
     val_dataset = ArtLoader(data_dir, split="test", transform=val_data_transforms, test_file="genre_val_with_dali.csv")
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    
     for i in range(num_epochs):
         train_loss, train_acc = train_unlearn_epoch(model, train_loader, optimizer)
         val_loss, val_acc = test(model, val_loader)
@@ -38,10 +42,21 @@ def unlearn_loss(data_dir, model, optimizer, batch_size, num_epochs):
 def learn_loss(data_dir, model, optimizer, batch_size, num_epochs):
     train_data_transforms = train_transforms(size=(224, 224))
     val_data_transforms = test_transforms(size=(224, 224))
+    
     train_dataset = ArtLoader(data_dir, split="train", transform=train_data_transforms, train_file="genre_train_with_dali.csv")
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    num_samples = 9600
+    sampler = SubsetRandomSampler(indices=list(range(len(train_dataset)))[:num_samples])
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=sampler)
+
     val_dataset = ArtLoader(data_dir, split="test", transform=val_data_transforms, test_file="genre_val_with_dali.csv")
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+
+    num_samples = 4800
+    sampler = SubsetRandomSampler(indices=list(range(len(val_dataset)))[:num_samples])
+    
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, sampler=sampler)
+    
     for i in range(num_epochs):
         train_loss, train_acc = train_learn_epoch(model, train_loader, optimizer)
         val_loss, val_acc = test(model, val_loader)
@@ -108,7 +123,12 @@ def inference_loop_learn(data_dir, model, batch_size) -> None:
         val_data_transforms = test_transforms(size=(224, 224))
         
         val_dataset = ArtLoader(data_dir, split="test", transform=val_data_transforms, test_file="genre_val_without_dali.csv")
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+
+        num_samples = 4800
+        sampler = SubsetRandomSampler(indices=list(range(len(val_dataset)))[:num_samples])
+
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, sampler=sampler)
+
         test_loss, test_acc = test(model, val_loader)
         print("Test Loss (Learn):", test_loss)
         print("Test Accuracy (Learn):", test_acc)
@@ -136,20 +156,22 @@ def main():
     checkpoint = torch.load('unlearn_art_loss_function_0.pt')
     my_resnet = ArtResnet()
     my_resnet.load_state_dict(checkpoint['model_state_dict'])
+    optimizer_config = {"optimizer_type": "adam", "lr": 1e-3, "weight_decay": 1e-5}
 
     #Stage 0
+    print("Stage 0")
     inference_loop_unlearn(data_dir="./data/", model=my_resnet, batch_size=32)
     inference_loop_learn(data_dir="./data/", model=my_resnet, batch_size=32)
 
     #Stage 1
-    optimizer_config = {"optimizer_type": "adam", "lr": 1e-4, "weight_decay": 1e-5}
+    print("Stage 1")
     optimizer = get_optimizer(my_resnet, optimizer_config)
-    unlearn_loss(model=my_resnet, optimizer=optimizer, data_dir="./data/", batch_size=32, num_epochs=1)
+    unlearn_loss(model=my_resnet, optimizer=optimizer, data_dir="./data/", batch_size=32, num_epochs=5)
     torch.save({'model_state_dict': my_resnet.state_dict(), 'optimizer_state_dict':optimizer.state_dict()}, './unlearn_art_loss_function_1.pt')
     inference_loop_learn(data_dir="./data/", model=my_resnet, batch_size=32)
 
     #Stage 2
-    optimizer_config = {"optimizer_type": "adam", "lr": 1e-3, "weight_decay": 1e-5}
+    print("Stage 2")
     checkpoint = torch.load('unlearn_art_loss_function_1.pt')
     my_resnet = ArtResnet()
     my_resnet.load_state_dict(checkpoint['model_state_dict'])
@@ -159,17 +181,17 @@ def main():
     inference_loop_unlearn(data_dir="./data/", model=my_resnet, batch_size=32)
     
     #Stage 3
-    optimizer_config = {"optimizer_type": "adam", "lr": 1e-4, "weight_decay": 1e-5}
+    print("Stage 3")
     checkpoint = torch.load('unlearn_art_loss_function_2.pt')
     my_resnet = ArtResnet()
     my_resnet.load_state_dict(checkpoint['model_state_dict'])
     optimizer = get_optimizer(my_resnet, optimizer_config)
-    unlearn_loss(model=my_resnet, optimizer=optimizer, data_dir="./data/", batch_size=32, num_epochs=1)
+    unlearn_loss(model=my_resnet, optimizer=optimizer, data_dir="./data/", batch_size=32, num_epochs=5)
     torch.save({'model_state_dict': my_resnet.state_dict(), 'optimizer_state_dict':optimizer.state_dict()}, './unlearn_art_loss_function_3.pt')
     inference_loop_unlearn(data_dir="./data/", model=my_resnet, batch_size=32)
     
     #Stage 4
-    optimizer_config = {"optimizer_type": "adam", "lr": 1e-3, "weight_decay": 1e-5}
+    print("Stage 4")
     checkpoint = torch.load('unlearn_art_loss_function_3.pt')
     my_resnet = ArtResnet()
     my_resnet.load_state_dict(checkpoint['model_state_dict'])
